aafa9eeece0afdee31aecfcbd4c3bc1620d7dcdd
hxc254 <66636147+hxc254@users.noreply.github.com>
2025-05-02 12:49:48 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export let PORT = 377; // 端口
 export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 // 代理
 export let PROXIES: Proxy[] = [{
-        domain: "easy-reverse-proxy-q7v10y8zk-logan-huangs-projects.vercel.app/",
+        domain: "www.example.com",
         url: "www.google.com",
         enable: true
 }];
